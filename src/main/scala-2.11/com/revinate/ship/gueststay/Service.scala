62470fb189c88b6ca387a36c9165b9b45a2668f9
Jorge Lee <jrg.lee@gmail.com>
2016-10-03 11:59:42 -0700

    Remove unnecessary companion object

--- src/main/scala-2.11/com/revinate/ship/gueststay/Service.scala
+++ src/main/scala-2.11/com/revinate/ship/gueststay/Service.scala
@@ -3,8 +3,6 @@ package com.revinate.ship.gueststay
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.revinate.ship.common.{MonetaryAmount, TimeSpan}
 
-object Service {}
-
 case class Service(
     inventoryCode: String,
     rateCode: String,
