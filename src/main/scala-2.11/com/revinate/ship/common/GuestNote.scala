371b4a1aad9b401dd99726ce01a4309f7a228314
Jorge Lee <jrg.lee@gmail.com>
2016-10-18 15:47:23 -0700

    Reformat code

--- src/main/scala-2.11/com/revinate/ship/common/GuestNote.scala
+++ src/main/scala-2.11/com/revinate/ship/common/GuestNote.scala
@@ -11,5 +11,7 @@ import java.time.OffsetDateTime
   */
 case class GuestNote(
     title: Option[String] = None,
-    text: String, `type`: Option[String] = None, time: Option[OffsetDateTime] = None
+    text: String,
+    `type`: Option[String] = None,
+    time: Option[OffsetDateTime] = None
 )
